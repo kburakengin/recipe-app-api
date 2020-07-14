59305c16746bce8d76f4fafb850e3b51821953f4
Kadir Burak Engin <kburakengin@gmail.com>
2020-07-14 11:56:49 +0300

    added recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from ..models import Tag, Ingredient
+from ..models import Tag, Ingredient, Recipe
 
 
 def sample_user(email='test@kburakengin.com', password='test123'):
@@ -57,7 +57,18 @@ class ModelTests(TestCase):
         """Test the ingredient string representation"""
         ingredient = Ingredient.objects.create(
             user=sample_user(),
-            name='Cocumber'
+            name='Cucumber'
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and mushroom sauce',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
