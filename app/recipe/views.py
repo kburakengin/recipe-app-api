12dfe075820cefe605e431697e2f5970d327e10a
Kadir Burak Engin <kburakengin@gmail.com>
2020-07-14 16:26:18 +0300

    added custom recipe detail view

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from core.models import Tag, Ingredient
-from .serializers import TagSerializer, IngredientSerializer
+from core.models import Tag, Ingredient, Recipe
+from .serializers import (TagSerializer,
+                          IngredientSerializer,
+                          RecipeSerializer,
+                          RecipeDetailSerializer)
 
 
 class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
@@ -31,3 +34,22 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
     """Manage ingredients in the database"""
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage Recipe in the database"""
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
+
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user"""
+        return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        """Return appropriate serializer class"""
+        if self.action == 'retrieve':
+            return RecipeDetailSerializer
+
+        return self.serializer_class
